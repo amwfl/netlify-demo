f69ecc7dbcf9d79121292d297cb629f5ed3262d4
Andrew Worcester <andrew@amwmedia.com>
2018-05-15 10:49:31 -0400

    updates to the hello world api, updates to npm start

--- functions/hello.js
+++ functions/hello.js
@@ -1,9 +1,14 @@
 export function handler(event, context, callback) {
 	callback(null, {
 		statusCode: 200,
-		body: "Hello, World --- <br>"
-			+ JSON.stringify(event)
-			+ "<br>"
-			+ JSON.stringify(context)
+		body: 'Hello World'
 	});
+
+	// callback(null, {
+	// 	statusCode: 200,
+	// 	body: "Hello, World\n\n"
+	// 		+ JSON.stringify(event, undefined, 2)
+	// 		+ "\n\n-------------------\n\n"
+	// 		+ JSON.stringify(context, undefined, 2)
+	// });
 }
