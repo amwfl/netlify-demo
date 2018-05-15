2469df1573b43fe0e3eb65520713ef307dc52bc0
Andrew Worcester <andrew@amwmedia.com>
2018-05-15 09:55:02 -0400

    added weather scene and did some basic styling

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import {getLoc} from './util/geo-location';
+const $ = window.jQuery;
 
-// testing...
-console.log('testing');
+$('#weather')
+	.attr('height', $(window).height() * 0.7)
+	.attr('width', $(window).width() * 0.7);
 
 getLoc().then(({coords}) => {
 	const {latitude: lat, longitude: lon} = coords;
-	jQuery.getJSON(`/.netlify/functions/current-weather?lat=${lat}&lon=${lon}`)
+	$.getJSON(`/.netlify/functions/current-weather?lat=${lat}&lon=${lon}`)
 		.then(res => {
-			console.log(res);
-			jQuery('#title').text(res.currently.summary);
+			console.log('WEATHER RESPONSE', res);
+			$('#title').text(res.currently.summary);
 
-			var skycons = new Skycons({"color": "black"});
+			var skycons = new Skycons({"color": "white"});
 			// you can add a canvas by it's ID...
 			skycons.add("weather", res.currently.icon);
 
